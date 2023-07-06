bd24183051fe3d73436180aaa24103ad687a6fa3
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-07-06 12:30:30 +0300

    3. Initialize Firebase (to Use all Firebase Services such as Cloud Firestore)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const ChatApp());
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const ChatApp());
+}
 
 const appName = 'Chat';
 
