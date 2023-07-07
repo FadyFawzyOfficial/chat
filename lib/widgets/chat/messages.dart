31c8befaf93a538c8fd8996e22adb159e2de95c2
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-07-07 16:08:56 +0300

    8. Display Username in the Message Bubble Widget (has a flaw with FutureBuilder)

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -31,6 +31,7 @@ class Messages extends StatelessWidget {
               message: currentMessage['text'],
               isOwner: currentMessage['userId'] ==
                   FirebaseAuth.instance.currentUser!.uid,
+              userId: currentMessage['userId'],
               //! Now we need a unique key, we can create it with the ValueKey()
               //! and use some unique value, and every message here has a unique
               //! value. It has a unique document ID so we can reach out to chat
